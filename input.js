2c8d53934016f060153d3ec4626bdf8814604d71
Charlie C <zbyabqrc@pm.me>
2023-04-29 04:34:09 +0000

    added and fixed setInterval for auto movement and adjustable speed.

--- input.js
+++ input.js
@@ -1,34 +1,50 @@
 // Stores the active TCP connection object.
 let connection;
+// interval variable is stored and cleared so one interval is running at a time.
+let interval;
 
 // handles user input
 const handleUserInput = function () {
   const stdin = process.stdin;
     stdin.on('data', (key) => {
-      
-      // if key === ctrl + c then exit game
+
+      // ctrl + c to exit game
       if (key === '\u0003') {
         console.log('exiting game')
         process.exit();
       }
 
+      // built-in js function clears interval timer
+      clearInterval(interval);
+
       // wasd movements
-      if (key === 'w') {  
-          connection.write("Move: up")  
+      if (key === 'w') {
+        // sets interval of movements 
+        interval = setInterval(() => {
+          connection.write("Move: up")
+          // change number to adjust speed
+        }, 100);
       }
-            
+      
       if (key === 'a') {
+        interval = setInterval(() => {
           connection.write("Move: left")
+        }, 100);
       }
-            
+
       if (key === 's') {
+        interval = setInterval(() => {
           connection.write("Move: down")
+        }, 100);
       }
-            
+
       if (key === 'd') {
+        interval = setInterval(() => {
           connection.write("Move: right")
+        }, 100);
       }
     })
+
 
 };
 
