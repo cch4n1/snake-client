cc31b70d3ddc83abde96df085e2f7687d38d7701
Charlie C <zbyabqrc@pm.me>
2023-04-29 05:05:42 +0000

    added some comments

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
-const IP = "localhost";
-const PORT = 50541;
-const NAME = "SNK";
+const IP = "localhost"; // IP Address
+const PORT = 50541; // Port #
+const NAME = "SNK"; // Name (3 Letters only)
 
 module.exports = { IP, PORT, NAME };
