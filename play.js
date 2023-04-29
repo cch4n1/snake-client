625392ed4a4c3793dbce3e0d6d2b36cc95903ca0
Charlie C <zbyabqrc@pm.me>
2023-04-29 00:40:18 +0000

    created setupInput and handleUserInput functions to help establish UI where players can send keyboard commands to preform an action. Added feature to exit game with 'ctrl + c'.

--- play.js
+++ play.js
@@ -1,4 +1,32 @@
 const { connect } = require("./client");
 
+const handleUserInput = function () {
+  const stdin = process.stdin;
+    stdin.on('data', (key) => {
+      if (key === '\u0003') {
+        console.log('exiting game')
+        process.exit();
+      }
+    })
+
+  // your code here
+  // const moveUp = function () {
+  //   conn.write("Move: up")
+  // }
+  // setInterval(moveUp, 100)
+};
+
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
 console.log("Connecting ...");
 connect();
+
+setupInput();
